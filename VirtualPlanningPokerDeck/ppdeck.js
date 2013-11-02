c9d91ae913445a4cc5b30797fea845db15bdf242
Juha Autero <jautero@iki.fi>
2013-11-03 00:41:39 +0200

    Testing creating named deck and deck from deck data,

--- VirtualPlanningPokerDeck/ppdeck.js
+++ VirtualPlanningPokerDeck/ppdeck.js
@@ -1,3 +1,20 @@
-function VirtualPlanningPokerDeck(){
-    
+var Decks={
+    reaktor: {
+        back: "",
+        cards: [" ","0","\u00BD","1","2","3","5","8","13","20","40","100","?","\u2615"]
+    }
+}
+
+function VirtualPlanningPokerDeck(deck){
+    this.setupDeck=function(deckdata) {
+        this.back=deckdata.back;
+        this.cards=deckdata.cards;
+    }
+    if (typeof deck == "string") {
+        if (deck in Decks) {
+            this.setupDeck(Decks[deck]);
+        }
+    } else {
+        this.setupDeck(deck);
+    }
 };
--- tst/ppdeck-tests.js
+++ tst/ppdeck-tests.js
@@ -1,5 +1,9 @@
 module("VirtualPlanningPokerDeck");
 test("create deck",function(){
-    expect(0);
-   var deck=new VirtualPlanningPokerDeck(); 
+   var deck=new VirtualPlanningPokerDeck("reaktor");
+   equal(deck.cards.length,14,"reaktor deck has 14 cards");
 });
+test ("create simple deck",function(){
+    var deck=new VirtualPlanningPokerDeck({back:"",cards:["1","2","3","4"]});
+    equal(deck.cards.length,4,"Deck does has four cards");
+})
