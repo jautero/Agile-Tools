f3bdad0fcfb95bbba643f6ab4a9642959ac2bbc3
Juha Autero <jautero@iki.fi>
2013-11-18 22:21:52 +0200

    Added Work Breakdown Sampler

---
+++ WorkBreakdownSampler/WorkBreakdownSampler.js
@@ -0,0 +1,6 @@
+//
+// 1. Main window has statistics and settings
+// It pops up a window that has buttons for every category
+// Settings: Add category, delete category, interval
+// Data is stored in local storage (settings and samples)
+// Main page has also button to clear samples.
