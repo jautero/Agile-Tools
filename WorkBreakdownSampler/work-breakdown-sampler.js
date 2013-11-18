58bce2fd908a88fe03daa03cd14e36b1cbafd873
Juha Autero <jautero@iki.fi>
2013-11-18 23:54:38 +0200

    Added WorkBreakdownSampler
    Added WorkBreakdownSampler/work-breakdown-sampler.js.
    Added tst/work-breakdown-sampler-test.js.
    Added both to tst/index.html

---
+++ WorkBreakdownSampler/work-breakdown-sampler.js
@@ -0,0 +1,9 @@
+var WBSdata={samples:{},total:0};
+
+function sample(category) {
+    if (WBSdata.samples[category] == undefined) {
+        WBSdata.samples[category]=0;
+    } 
+    WBSdata.samples[category]++;
+    WBSdata.total++;
+}
---
+++ tst/work-breakdown-sampler-test.js
@@ -0,0 +1,18 @@
+module("WorkBreakdownSampler", {
+    setup: function () {
+    WBSdata={samples:{},total:0};
+}});
+test("test_sample",function () {
+    sample("test1");
+    equal(WBSdata.samples.test1,1,"samples of test1 should be 1 and not "+WBSdata.samples.test1);
+    equal(WBSdata.total,1,"total should be 1 and not "+WBSdata.total);
+});
+
+test("test_three_samples",function () {
+    sample("test1");
+    sample("test1");
+    sample("test2");
+    equal(WBSdata.samples.test1,2,"samples of test1 should be 2 and not "+WBSdata.samples.test1);
+    equal(WBSdata.samples.test2,1,"samples of test2 should be 1 and not "+WBSdata.samples.test2);
+    equal(WBSdata.total,3,"total should be 3 and not "+WBSdata.total);    
+});
